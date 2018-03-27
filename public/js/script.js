41ff00787651a80be600a84942415dfaf5ef38f3
Achmad Nurisman <sebutsajasayaiman@gmail.com>
2018-03-27 13:41:20 +0700

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const socket = io();
+const outputYou = document.querySelector('.output-you');
+const outputBot = document.querySelector('.output-bot');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+document.querySelector('button').addEventListener('click', function () {
+    recognition.start();
+});
+
+recognition.addEventListener('speechstart', function () {
+    console.log('Speech has been detected.');
+});
+
+recognition.addEventListener('result', function (e) {
+    console.log('Result has been detected.');
+
+    let last = e.result.length - 1;
+    let text = e.result[last][0].transcript;
+
+    outputYou.textContent = text;
+    console.log('Confidence: ' + e.result[0][0].confidence);
+    socket.emit('chat message', text);
+});
+
+recognition.addEventListener('speechend', function () {
+    recognition.stop();
+});
+
+recognition.addEventListener('error', function (e) {
+    outputBot.textContent = 'Error: ' + e.error;
+});
+
+function  synthVoice(text) {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text = text;
+    synth.speak(utterance);
+}
+
+socket.on('bot reply', function (replyText) {
+    synthVoice(replyText);
+
+    if(replyText == '') replyText = '(No Answer...)';
+    outputBot.textContent = replyText;
+});
